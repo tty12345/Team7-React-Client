0da3d149cca61c4f558a6c876677cc5ba2b5b7fc
Teck Yi <teckyi1993@gmail.com>
2021-08-20 18:32:45 +0800

    wtf

--- src/Component/CarList.js
+++ src/Component/CarList.js
@@ -2,6 +2,27 @@
 import React, { Component } from "react";
 import CarPostDataService from "../Services/CarPostService.js";
 import { Link } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const data = [
+  {id: 1, name: 'Gob', value: '2'},
+  {id: 2, name: 'Buster', value: '5'},
+  {id: 3, name: 'George Michael', value: '4'}
+];
+const columns = [{
+  dataField: 'id',
+  text: 'Product ID'
+}, {
+  dataField: 'name',
+  text: 'Product Name'
+}, {
+  dataField: 'value',
+  text: 'Product value'
+}];
 
 export default class CarList extends Component {
     constructor(props) {
