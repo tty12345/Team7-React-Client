a7465c3216242cb6eac0effbbf21c3a0e3f3cce2
Jingxia Wu <e0641543@u.nus.edu>
2021-08-26 21:47:27 +0800

    css

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -48,7 +48,8 @@ export default class HomePage extends Component {
 
         return (
             <div className="App">
-            <div className="jumbotron feature">
+               
+            {/* <div className="jumbotron feature">
               <div className="container">
                 <h1><span className="glyphicon glyphicon-equalizer"></span>Prices Updated Monthly</h1>
                 <p>Using algorithms to determine your car's best selling price</p>
@@ -56,7 +57,16 @@ export default class HomePage extends Component {
                   Get Estimate
                 </Link></button>
               </div>
-            </div>
+            </div> */}
+            <div class="container-fluid page-header">
+        <h1><span className="glyphicon glyphicon-equalizer"></span><font color = "#ffffff" >Prices Updated Monthly</font></h1>
+                <p>Using algorithms to determine your car's best selling price</p>
+                <button><Link to={"/Estimate/"}>
+                  Get Estimate
+                </Link></button>
+                </div>
+        
+
             <div className="container">
               <div className="row">
                 <h2>{title}</h2>
@@ -120,9 +130,77 @@ export default class HomePage extends Component {
                       </article>
                   </div>
               </div>
-            <div>
+            {/* <div>
               <DefaultCarsTable />
+            </div> */}
+
+<div class="container-fluid py-5">
+        <div class="container pt-5 pb-3">
+            <h1 class="display-4 text-uppercase text-center mb-5">Welcome To <span class="text-primary">Car Exchange</span></h1>
+            <div class="row justify-content-center">
+                <div class="col-lg-10 text-center">
+                    <img class="w-75 mb-4" src="images/about.png" alt=""/>
+                    <p>Justo et eos et ut takimata sed sadipscing dolore lorem, et elitr labore labore voluptua no rebum sed, stet voluptua amet sed elitr ea dolor dolores no clita. Dolores diam magna clita ea eos amet, amet rebum voluptua vero vero sed clita accusam takimata. Nonumy labore ipsum sea voluptua sea eos sit justo, no ipsum sanctus sanctus no et no ipsum amet, tempor labore est labore no. Eos diam eirmod lorem ut eirmod, ipsum diam sadipscing stet dolores elitr elitr eirmod dolore. Magna elitr accusam takimata labore, et at erat eirmod consetetur tempor eirmod invidunt est, ipsum nonumy at et.</p>
+                </div>
             </div>
+            <div class="row mt-3">
+                <div class="col-lg-4 mb-2">
+                    <div class="d-flex align-items-center bg-light p-4 mb-4" styles="height: 150px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary ml-n4 mr-4" styles="width: 100px; height: 100px;">
+                            <i class="fa fa-2x fa-headset text-secondary"></i>
+                        </div>
+                        <h4 class="text-uppercase m-0">24/7 Car Sale Support</h4>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-2">
+                    <div class="d-flex align-items-center bg-secondary p-4 mb-4" styles="height: 150px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary ml-n4 mr-4" styles="width: 100px; height: 100px;">
+                            <i class="fa fa-2x fa-car text-secondary"></i>
+                        </div>
+                        <h4 class="text-light text-uppercase m-0">Get estimate selling price</h4>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-2">
+                    <div class="d-flex align-items-center bg-light p-4 mb-4" styles="height: 150px;">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary ml-n4 mr-4" styles="width: 100px; height: 100px;">
+                            <i class="fa fa-2x fa-map-marker-alt text-secondary"></i>
+                        </div>
+                        <h4 class="text-uppercase m-0">Lots of car models</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+           
+            <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="row mx-0">
+                <div class="col-lg-6 px-0">
+                    <div class="px-5 bg-secondary d-flex align-items-center justify-content-between" background styles="height: 350px;">
+                        <img class="img-fluid flex-shrink-0 ml-n5 w-50 mr-4" src="images/banner-left.png" alt=""/>
+                        <div class="text-right">
+                            <h3 class="text-uppercase text-light mb-3">Want to sell your car?</h3>
+                            <p class="mb-4">Lorem justo sit sit ipsum eos lorem kasd, kasd labore</p>
+                            <a class="btn btn-primary py-2 px-4" href="">Start Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 px-0">
+                    <div class="px-5 bg-dark d-flex align-items-center justify-content-between" styles="height: 350px;">
+                        <div class="text-left">
+                            <h3 class="text-uppercase text-light mb-3">Looking for a car?</h3>
+                            <p class="mb-4">Lorem justo sit sit ipsum eos lorem kasd, kasd labore</p>
+                            <a class="btn btn-primary py-2 px-4" href="">Start Now</a>
+                        </div>
+                        <img class="img-fluid flex-shrink-1 mr-n5 w-50 ml-4" src="images/banner-right.png" alt=""/>
+                   </div> 
+                </div>
+            </div>
+        </div>
+    </div>
+   
+
             <footer>
               <div className="footer-blurb">
                 <div className="container">
