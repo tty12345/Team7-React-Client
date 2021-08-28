be72d5d53d74c93fdb1c4457af214c42b81501e6
Teck Yi <teckyi1993@gmail.com>
2021-08-29 00:37:01 +0800

    push some stuff

--- src/Component/PrefCarList.js
+++ src/Component/PrefCarList.js
@@ -1,25 +1,25 @@
 
-import React, { Component } from "react";
-import CarPostDataService from "../Services/CarPostService.js";
-import '../App.css';
-import{PrefCarsTable} from "./table/PrefCarsTable";
-import{WatchListTable} from "./table/WatchListTable";
+// import React, { Component } from "react";
+// import CarPostDataService from "../Services/CarPostService.js";
+// import '../App.css';
+// import{PrefCarsTable} from "./table/PrefCarsTable";
+// import{WatchListTable} from "./table/WatchListTable";
 
-export default class PrefCarList extends Component {
-    constructor(props) {
-        super(props);
+// export default class PrefCarList extends Component {
+//     constructor(props) {
+//         super(props);
   
-      }
+//       }
 
 
   
-    render(){
-        return(
-              <div>
-                <PrefCarsTable/>
-                <WatchListTable/>
-              </div>
-        )
-    }
+//     render(){
+//         return(
+//               <div>
+//                 <PrefCarsTable/>
+//                 <WatchListTable/>
+//               </div>
+//         )
+//     }
   
-}
+// }
